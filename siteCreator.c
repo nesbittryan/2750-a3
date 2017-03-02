cb97132e161389dd8a4397bcae880248319ed6a2
Ryan Nesbitt <nesbittr@mail.uoguelph.ca>
2017-03-02 09:00:14 -0500

    site creator file input

--- siteCreator.c
+++ siteCreator.c
@@ -1,6 +1,17 @@
 #include "siteCreator.h"
 
 int main(int argc, char **argv) {
-    printf("%d\n", argc);
+    char * filename;
+    if(argc != 2) {
+        printf("Default file: <config> being used\n");
+        filename = malloc(sizeof(char) * 7);
+        strcpy(filename, "config");
+    } else {
+        printf("Input file: <%s> being used\n", argv[1]);
+        filename = malloc(strlen(argv[1]) + 1);
+        strcpy(filename, argv[1]);
+    }
+
+    printf("%s\n", filename);
     return(0);
 }
