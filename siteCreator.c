00885cd0bbd96803261d5beb494ccab221045aaa
Ryan Nesbitt <nesbittr@mail.uoguelph.ca>
2017-03-02 18:02:05 -0500

    modifications

--- siteCreator.c
+++ siteCreator.c
@@ -1,13 +1,59 @@
 #include "siteCreator.h"
 
+void parseLine(char *buffer);
+
 int main(int argc, char **argv) {
-    char * filename;
     if(argc != 2) {
         printf("Include Filename\n");
         return(-1);
     }
-    filename = malloc(strlen(argv[1]) + 1);
+    char *filename = malloc(strlen(argv[1]) + 1);
     strcpy(filename, argv[1]);
-    printf("%s\n", filename);
+    char *buffer = malloc(sizeof(char) * 1000);
+    FILE *fp = fopen(filename, "r");
+    if(fp == NULL) {
+        printf("Invalid File\n");
+        return(-1);
+    }
+
+    while(fgets(buffer, 999,fp)) {
+        buffer[strlen(buffer) - 1] = '\0';
+        parseLine(buffer);
+    }
+    fclose(fp);
+
+    free(buffer);
+    free(filename);
     return(0);
+}
+
+void parseLine(char *buffer) {
+    char *tok = strtok(buffer, ")");
+    while(tok != NULL) {
+        if(tok[0] == '.' && tok[2] == '(') {
+            switch(tok[1]) {
+                case 'h':
+                    break;
+                case 't':
+                    break;
+                case 'd':
+                    break;
+                case 'l':
+                    break;
+                case 'p':
+                    break;
+                case 'e':
+                    break;
+                case 'b':
+                    break;
+                case 'i':
+                    break;
+                case 'r':
+                    break;
+                default:
+
+            }
+        }
+        tok = strtok(NULL, ")");
+    }
 }
